a0ed190f0133efeab760e2d9e73f80f93adc5e5c
Frank Riezebos <info@frankriezebos.nl>
2021-06-29 14:36:56 +0200

    Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="https://i.imgur.com/UYP2umJ.png" alt="Vue">
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
