6d77c242b5fdfabb7f33455a00dbcddc72a2bc50
Frank Riezebos <info@frankriezebos.nl>
2021-06-29 14:38:16 +0200

    vuex store

---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+            id: 'balut',
+            title: 'What is Balut?',
+            content: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat.'
+        },
+        {
+            id: 'frank',
+            title: 'Who is Frank?',
+            content: 'Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Sed porttitor lectus nibh. Sed porttitor lectus nibh.'
+        },
+        {
+            id: 'lilly',
+            title: 'Who is Lilly?',
+            content: 'Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.'
+        }
+    ]
+})
